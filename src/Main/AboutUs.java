aa6bf5508e33f37ae4dcaef5a833c600bd121d4d
Albert Negoro <56742226+albertn89@users.noreply.github.com>
2022-04-03 20:09:20 -0700

    Update AboutUs.java

--- src/Main/AboutUs.java
+++ src/Main/AboutUs.java
@@ -20,10 +20,7 @@ public class AboutUs {
     private Scene scene;
     private Parent root;
     private Label accountLabel;
-
-
-
-
+    
     public void homeButton(MouseEvent mouseEvent) throws IOException {
         if (Login.custLoggedIn == true){
             root = FXMLLoader.load(getClass().getResource("../Scenes/homePage.fxml"));
@@ -46,16 +43,24 @@ public class AboutUs {
     }
 
     public void menuButton(MouseEvent mouseEvent) throws IOException {
-        stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
-        menuGUI menuScene = new menuGUI(stage);
-        stage.setScene(menuScene.getScene());
-        stage.show();
+        if (Login.custLoggedIn == true) {
+            stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+            menuGUI menuScene = new menuGUI(stage);
+            stage.setScene(menuScene.getScene());
+            stage.show();
+        } else {
+            root = FXMLLoader.load(getClass().getResource("../Scenes/login.fxml"));
+            stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
     public void cartButton(MouseEvent mouseEvent) throws IOException {
-        if (Login.custLoggedIn == true){
+        if (Login.custLoggedIn == true) {
             root = FXMLLoader.load(getClass().getResource("../Scenes/cart.fxml"));
-            
+
         } else {
             root = FXMLLoader.load(getClass().getResource("../Scenes/login.fxml"));
         }
@@ -67,11 +72,12 @@ public class AboutUs {
     }
 
     public void accountButton(MouseEvent mouseEvent) throws IOException {
-        if (Login.custLoggedIn == true){
+        if (Login.custLoggedIn == true) {
             root = FXMLLoader.load(getClass().getResource("../Scenes/profile.fxml"));
         } else {
             root = FXMLLoader.load(getClass().getResource("../Scenes/login.fxml"));
         }
+
         stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
