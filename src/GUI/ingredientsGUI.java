aa7b27302b92788adc2e71e82f5a7c0e678aa0ba
jjansen3 <99362665+jjansen3@users.noreply.github.com>
2022-04-03 17:01:33 -0700

    Menu and ingredients
    Menu and ingredients page added to project

---
+++ src/GUI/ingredientsGUI.java
@@ -0,0 +1,163 @@
+package GUI;
+
+import Main.Main.*;
+import Main.*;
+import Main.MenuItem;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+//import javafx.scene.control.MenuItem;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+// ingredients for individual item
+public class ingredientsGUI {
+    Scene ingredientsScene;
+
+    public ingredientsGUI(Stage primaryStage) {
+
+        // Setting up the main grid for the menu
+        GridPane menuGrid = new GridPane();
+        menuGrid.setVgap(20);
+        menuGrid.setHgap(20);
+        menuGrid.setAlignment(Pos.CENTER);
+
+
+        // title - custom font not working
+        Text title = new Text();
+        title.setText("Oregano's Trattoria");
+        title.setX(100);
+        title.setY(100);
+        //Font customFont = Font.loadFont("../Assets/BonheurRoyale-Regular", 100);
+        //title.setFont(customFont);
+        title.setFont(Font.font("Verdana", FontWeight.BOLD, 35));
+
+        // adding menu items to GUI
+        int i = 0;
+        int j = 0;
+        for(MenuItem m : Main.menu.items) {
+            StackPane itemGUI = new StackPane();
+            Rectangle rect = new Rectangle();
+            rect.setWidth(400);
+            rect.setHeight(150);
+            rect.setStroke(Color.LIGHTGRAY);
+            rect.setStrokeWidth(3.0);
+            rect.setFill(Color.WHITE);
+            ImageView image = new ImageView(m.image);
+            image.setFitHeight(100);
+            image.setFitWidth(100);
+            image.setX(10);
+            Text itemName = new Text(m.name);
+            itemName.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
+            Text itemIngredients = new Text(m.ingredients);
+            Text price = new Text("$" + String.format("%.2f", m.price));
+            price.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
+            price.setFill(Color.GREEN);
+            itemIngredients.setWrappingWidth(220);
+            itemGUI.getChildren().addAll(rect, image, itemName, itemIngredients, price);
+            itemGUI.setMargin(image, new Insets(0, 250, 0, 0));
+            itemGUI.setAlignment(itemName, Pos.CENTER_LEFT);
+            itemGUI.setMargin(itemName, new Insets(0, 0, 75, 157));
+            itemGUI.setAlignment(itemIngredients, Pos.TOP_CENTER);
+            itemGUI.setMargin(itemIngredients, new Insets(60, 0, 0, 132));
+            itemGUI.setAlignment(price, Pos.CENTER_RIGHT);
+            itemGUI.setMargin(price, new Insets(0, 25, 75, 0));
+            menuGrid.add(itemGUI, i, j);
+            i++;
+
+            // new row
+            if (i > 2) {
+                i = 0;
+                j++;
+            }
+        }
+
+        //Blurs the background to focus on adding the item
+        javafx.scene.effect.BoxBlur bb = new BoxBlur();
+        bb.setWidth(10);
+        bb.setHeight(10);
+        bb.setIterations(3);
+        menuGrid.setEffect(bb);
+
+        // Ingredients panel
+        Rectangle rect = new Rectangle();
+        rect.setWidth(650);
+        rect.setHeight(300);
+        rect.setStroke(Color.GRAY);
+        rect.setStrokeWidth(3.0);
+        rect.setFill(Color.WHITE);
+        StackPane ingredientsGUI = new StackPane();
+        Text name = new Text(Main.menu.items.get(0).name.toString()); // have to refer to items in arrList (Main.menu.items)
+        name.setFont(Font.font("Verdana", 18));
+        Text ingredientsTitle = new Text("Ingredients");
+        ingredientsTitle.setFont(Font.font("Verdana", 18));
+        Text ingredientsText = new Text(Main.menu.items.get(0).ingredients.toString()); // MenuItem ingredients
+        ingredientsText.setFont(Font.font("Verdana", 12));
+        Text size = new Text(Main.menu.items.get(0).size.toString());
+        size.setFont(Font.font("Verdana", 18));
+        Text itemSize = new Text("Item size"); // MenuItem size
+        itemSize.setFont(Font.font("Verdana", 18));
+        ImageView image = new ImageView(Main.menu.items.get(0).image); // image
+        image.setFitHeight(200);
+        image.setFitWidth(200);
+        Button menuImage = new Button();
+        menuImage.setPrefSize(200,200);
+        menuImage.setGraphic(image);
+        Button addCheckoutButton = new Button();
+        addCheckoutButton.setText("Add to checkout");
+        Button backButton = new Button();
+        backButton.setText("Back");
+        ingredientsGUI.setMargin(name, new Insets(0, 100, 185, 125));
+        ingredientsGUI.setMargin(ingredientsTitle, new Insets(0, 0, 35, 50));
+        ingredientsGUI.setMargin(ingredientsText, new Insets(50, 0, 0, 250));
+        ingredientsGUI.setMargin(addCheckoutButton, new Insets(185, 0, 0, 350));
+        ingredientsGUI.setMargin(size,new Insets(200, 5, 385, 305)); // above checkout button
+        ingredientsGUI.setMargin(itemSize,new Insets(0, 5, 185, 100)); // above checkout button
+        ingredientsGUI.setMargin(menuImage, new Insets(0, 370, 0, 0));
+        ingredientsGUI.setMargin(backButton, new Insets(180, 0, 0, 290));
+        ingredientsGUI.setAlignment(backButton, Pos.TOP_LEFT);
+        ingredientsText.setWrappingWidth(300);
+        ingredientsGUI.getChildren().addAll(rect, name, ingredientsText, ingredientsTitle, addCheckoutButton, size, menuImage, backButton);
+
+
+        // add to stack pane
+        StackPane sp = new StackPane();
+        sp.setMargin(ingredientsGUI, new Insets(20, 20, 20, 20));
+        ObservableList list = sp.getChildren();
+        list.addAll(menuGrid, ingredientsGUI);
+        ingredientsScene = new Scene(sp);
+
+
+        //Switches to the menu page when the backButton pressed
+        backButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                menuGUI menuPage = new menuGUI(primaryStage);
+                primaryStage.setScene(menuPage.getScene());
+            }
+        });
+    }
+
+    //Returns this scene
+    public Scene getScene() {
+        return ingredientsScene;
+    }
+
+
+}
---
+++ src/GUI/menuGUI.java
@@ -0,0 +1,249 @@
+package GUI;
+
+import Main.Main;
+import Main.MenuItem;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+//import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
+// main menu GUI
+public class menuGUI {
+
+    Scene menuScene;
+
+    public menuGUI(Stage primaryStage) {
+
+        // Setting up the main grid for the menu
+        GridPane menuGrid = new GridPane();
+        menuGrid.setVgap(20);
+        menuGrid.setHgap(20);
+        menuGrid.setAlignment(Pos.CENTER);
+
+        // Setting buttons for top banner / search bar navigation
+        Button homeButton = new Button();
+        Button aboutButton = new Button();
+        Button cartButton = new Button();
+        Button loginButton = new Button();
+        ImageView homePic = new ImageView("Assets/Home_icon.png");
+        ImageView aboutPic = new ImageView("Assets/Contact_icon.png");
+        ImageView cartPic = new ImageView("Assets/Cart_icon.png");
+        ImageView loginPic = new ImageView("Assets/Account_icon.png");
+        // picture size
+        homePic.setFitHeight(100);
+        homePic.setFitWidth(120);
+        aboutPic.setFitHeight(100);
+        aboutPic.setFitWidth(120);
+        cartPic.setFitHeight(100);
+        cartPic.setFitWidth(120);
+        loginPic.setFitHeight(100);
+        loginPic.setFitWidth(120);
+        // set button graphics
+        homeButton.setGraphic(homePic);
+        aboutButton.setGraphic(aboutPic);
+        cartButton.setGraphic(cartPic);
+        loginButton.setGraphic(loginPic);
+
+
+        // title - custom font not working
+        Text title = new Text();
+        title.setText("Oregano's Trattoria");
+        //Font customFont = Font.loadFont("../Assets/BonheurRoyale-Regular", 100);
+        //title.setFont(customFont);
+        title.setFont(Font.font("Verdana", FontWeight.BOLD, 35));
+
+        // Button text
+
+        Text homeText = new Text();
+        homeText.setText("Home");
+        homeText.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
+
+        Text aboutText = new Text();
+        homeText.setText("About Us");
+        /*
+        Text cartText = new Text();
+        homeText.setText("Cart");
+        Text loginText = new Text();
+        homeText.setText("Sign In");
+         */
+
+
+        // adding menu items to GUI
+        int i = 0;
+        int j = 0;
+        for(MenuItem m : Main.menu.items) {
+            StackPane itemGUI = new StackPane();
+            Rectangle rect = new Rectangle();
+            rect.setWidth(400);
+            rect.setHeight(150);
+            rect.setStroke(Color.LIGHTGRAY);
+            rect.setStrokeWidth(3.0);
+            rect.setFill(Color.WHITE);
+            // ingredients page when clicked
+            rect.setOnMouseClicked(mouseEvent -> {
+                ingredientsGUI ingredientsPage = new ingredientsGUI(primaryStage);
+                primaryStage.setScene(ingredientsPage.getScene());
+            });
+            ImageView image = new ImageView(m.image);
+            image.setFitHeight(100);
+            image.setFitWidth(100);
+            image.setX(10);
+            Text itemName = new Text(m.name);
+            itemName.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
+            Text itemIngredients = new Text(m.ingredients);
+            Text price = new Text("$" + String.format("%.2f", m.price));
+            price.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
+            price.setFill(Color.GREEN);
+            itemIngredients.setWrappingWidth(220);
+            itemGUI.getChildren().addAll(rect, image, itemName, itemIngredients, price);
+            itemGUI.setMargin(image, new Insets(0, 250, 0, 0));
+            itemGUI.setAlignment(itemName, Pos.CENTER_LEFT);
+            itemGUI.setMargin(itemName, new Insets(0, 0, 75, 157));
+            itemGUI.setAlignment(itemIngredients, Pos.TOP_CENTER);
+            itemGUI.setMargin(itemIngredients, new Insets(60, 0, 0, 132));
+            itemGUI.setAlignment(price, Pos.CENTER_RIGHT);
+            itemGUI.setMargin(price, new Insets(0, 25, 75, 0));
+            menuGrid.add(itemGUI, i, j);
+            i++;
+
+            // new row
+            if (i > 2) {
+                i = 0;
+                j++;
+            }
+        }
+
+        //Switches to the home page when the homeButton pressed
+        homeButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //switch to main page
+                try {
+                    goToHome(event);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        //Switches the scene to about page when the aboutButton pressed
+        aboutButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //switch to about page
+                try {
+                    goToAbout(event);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        //Switches to cart page when the cartButton pressed
+        cartButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // cart page
+            }
+        });
+
+        //Switches to sign in page when the loginButton pressed
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //switch to main page
+                try {
+                    goToLogin(event);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        //Adding GridPane / buttons to the main stack pane
+        StackPane sp = new StackPane();
+        sp.setAlignment(menuGrid, Pos.BOTTOM_CENTER);
+        sp.setMargin(menuGrid, new Insets(150, 20, 20, 20));
+        sp.setAlignment(homeButton, Pos.TOP_LEFT);
+        sp.setMargin(homeButton, new Insets(20, 0, 0, 0));
+        sp.setAlignment(aboutButton, Pos.TOP_LEFT);
+        sp.setMargin(aboutButton, new Insets(20, 0, 0, 275));
+        sp.setAlignment(cartButton, Pos.TOP_RIGHT);
+        sp.setMargin(cartButton, new Insets(20, 275, 0, 0));
+        sp.setAlignment(loginButton, Pos.TOP_RIGHT);
+        sp.setMargin(loginButton, new Insets(20, 0, 0, 0));
+        sp.setAlignment(title, Pos.TOP_CENTER);
+        sp.setMargin(title, new Insets(50,0,0,0));
+        //sp.setMargin(homeText, new Insets(100,1150,675,0));
+        //sp.setMargin(aboutText, new Insets(100,850,675,0));
+
+        /*
+        sp.setAlignment(aboutText, Pos.TOP_LEFT);
+        sp.setMargin(aboutText, new Insets(150,50,0,0));
+        sp.setAlignment(cartText, Pos.TOP_LEFT);
+        sp.setMargin(cartText, new Insets(150,80,0,0));
+        sp.setAlignment(loginText, Pos.TOP_LEFT);
+        sp.setMargin(loginText, new Insets(150,100,0,0));
+         */
+        sp.getChildren().addAll(menuGrid, homeButton, aboutButton, cartButton, loginButton, title);
+        menuScene = new Scene(sp);
+
+    }
+
+    //Returns this scene
+    public Scene getScene() {
+        return menuScene;
+    }
+
+
+    public void goToHome(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../Scenes/homePage.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void goToAbout(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../Scenes/aboutUs.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void goToLogin(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../Scenes/login.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+}
