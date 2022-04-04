a84d1a47586463c70d68a836b8313c59df28a686
Albert Negoro <56742226+albertn89@users.noreply.github.com>
2022-04-03 17:23:12 -0700

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,25 +15,6 @@ public class Main {
     //Test Main.Menu
     public static Menu menu = new Menu(new ArrayList<>(Arrays.asList(item, item, item, item, item, item, item, item, item, item)));
 
-    package Main;
-
-import javafx.scene.image.Image;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class Main {
-
-    //Test item
-    static MenuItem item = new MenuItem("Lasagna", "Freshly baked lasagna with minced beef and Ricotta cheese. Finished with fresh basil.", "", 12, new Image("Assets/las.jpg"));
-
-    public static ArrayList<Customer> customers = new ArrayList<>();
-
-    //Test Main.Menu
-    public static Menu menu = new Menu(new ArrayList<>(Arrays.asList(item, item, item, item, item, item, item, item, item, item)));
-
     static Customer currCust = new Customer("og.trattoria@res.org", "John", "Smith", "oregano123", "refcode", "1234567890", "123 Sesame St", new ArrayList<Order>(), new Order(5, new ArrayList<>(Arrays.asList(item, item, item, item, item, item, item, item, item, item)), 0.0, "date") , new ArrayList<Coupon>(), new ArrayList<>());
-
-}
 
 }
