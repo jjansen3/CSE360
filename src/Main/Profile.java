a0224904b7d4e162ebe37f42bf16d4f33c4321f0
Albert Negoro <56742226+albertn89@users.noreply.github.com>
2022-04-03 21:26:31 -0700

    Update Profile.java

--- src/Main/Profile.java
+++ src/Main/Profile.java
@@ -2,9 +2,11 @@ package Main;
 
 import GUI.Start;
 import GUI.devMenuGUI;
+import GUI.menuGUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,33 +16,53 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import java.io.IOException;
-import GUI.menuGUI;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Profile {
+public class Profile implements Initializable {
 
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-
     @FXML
     private SplitMenuButton accountMenu;
     @FXML
-    public static Label displayfName, displaylName, displayEmail, displayAddr;
+    public Label displayName, displayEmail, displayAddr, displayPhone;
+    @FXML
+    public Label cDesc, cCode, cExp;
+    @FXML
+    public Label orderNum, orderDate, orderPrice;
 
 
-    /*
-    public static void displayInfo() {
-        for (int i = 0; i<Main.customers.size();i++) {
-            displayfName.setText(Main.customers.get(i).fName);
-            displaylName.setText(Main.customers.get(i).lName);
-            displayEmail.setText(Main.customers.get(i).email);
-            displayAddr.setText(Main.customers.get(i).address);
+    // Display user info
+    public void initialize(URL url, ResourceBundle rb){
+        // Display user's name and email
+        displayName.setText(Main.currCust.fName + " " + Main.currCust.lName);
+        displayEmail.setText(Main.currCust.email);
+
+        // Display user's phone number
+        if (Main.currCust.phoneNum != null) {
+            displayPhone.setText(Main.currCust.phoneNum);
         }
 
-    }
 
-     */
+        // Display user's Coupon
+        for (int i = 0; i < Main.currCust.coupons.size(); i++) {
+            cDesc.setText(Main.currCust.coupons.get(i).desc);
+            cCode.setText(Main.currCust.coupons.get(i).code);
+            cExp.setText(Main.currCust.coupons.get(i).valid);
+        }
+
+        // Display user's order history
+        for (int i = 0; i < Main.currCust.orders.size(); i++) {
+            orderNum.setText(String.valueOf(Main.currCust.orders.get(i).orderNum));
+            orderDate.setText(Main.currCust.orders.get(i).date);
+            orderPrice.setText(String.valueOf(Main.currCust.orders.get(i).total));
+        }
+
+
+    }
 
 
     // App Navigation
@@ -61,7 +83,7 @@ public class Profile {
     }
 
     public void goToMenu(MouseEvent mouseEvent) throws IOException {
-    	stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
         menuGUI menuScene = new menuGUI(stage);
         stage.setScene(menuScene.getScene());
         stage.show();
@@ -76,7 +98,7 @@ public class Profile {
     }
 
 
-    // Edit Profile
+    /*
     public void editProfile(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("../Scenes/editProfile.fxml"));
         stage = (Stage)accountMenu.getScene().getWindow();
@@ -84,6 +106,8 @@ public class Profile {
         stage.setScene(scene);
         stage.show();
     }
+     */
+
 
     // Dev Switch
     public void devSwitch(ActionEvent event) throws IOException {
@@ -98,7 +122,6 @@ public class Profile {
 
     // Exit options
     public void exitPrompt(ActionEvent event) throws IOException {
-        Login.custLoggedIn = false;
         root = FXMLLoader.load(getClass().getResource("../Scenes/exitPrompt.fxml"));
         stage = (Stage)accountMenu.getScene().getWindow();
         scene = new Scene(root);
